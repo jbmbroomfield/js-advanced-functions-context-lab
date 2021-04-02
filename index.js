8e6e3bb7cd3758fd0352aa3920ce697376d7ffa4
jim <jbmbroomfield@gmail.com>
2021-04-02 15:23:15 +1100

    Done

--- index.js
+++ index.js
@@ -1,4 +1,56 @@
-/* Your Code Here */
+function createEmployeeRecord(input) {
+    return {
+        firstName: input[0],
+        familyName: input[1],
+        title: input[2],
+        payPerHour: input[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+}
+
+function createEmployeeRecords(inputs) {
+    return inputs.map(input => createEmployeeRecord(input))
+}
+
+function createEvent(type,dateStamp) {
+    const dateStampArray = dateStamp.split(' ')
+    this.push({
+        type: type,
+        hour: parseInt(dateStampArray[1]),
+        date: dateStampArray[0],
+    })
+    return this
+}
+
+function createTimeInEvent(dateStamp) {
+    this.timeInEvents = createEvent.call(this.timeInEvents,'TimeIn',dateStamp)
+    return this
+}
+
+function createTimeOutEvent(dateStamp) {
+    this.timeOutEvents = createEvent.call(this.timeOutEvents,'TimeOut',dateStamp)
+    return this
+}
+
+function hoursWorkedOnDate(date) {
+    const timeIn = this.timeInEvents.find(event => event.date === date).hour
+    const timeOut = this.timeOutEvents.find(event => event.date === date).hour
+    return (timeOut - timeIn)/100
+}
+
+function wagesEarnedOnDate(date) {
+    return hoursWorkedOnDate.call(this,date) * this.payPerHour
+}
+
+function findEmployeeByFirstName(srcArray,firstName) {
+    return srcArray.find(record => record.firstName === firstName)
+}
+
+function calculatePayroll(srcArray) {
+    return srcArray.reduce((a,b) => a + allWagesFor.call(b),0)
+}
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
